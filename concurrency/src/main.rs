d688441d6fd426e21aab32a5a7eba412c78043ae
0ersted <zhsang@link.cuhk.edu.hk>
2020-01-30 17:48:59 +0800

    complete 16.4
    basic mutex

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,10 +1,11 @@
 use std::thread;
 use std::time::Duration;
-
 use std::sync::mpsc;
+use std::sync::{Mutex, Arc};
 
 fn main() {
     // 16.1 basic thread
+    println!("\nthreading!\n");
     let handle = thread::spawn(||{
         for i in 1..10 {
             println!("hi number {} from the spawned thread", i);
@@ -20,6 +21,7 @@ fn main() {
     handle.join().unwrap();
 
     // 16.2 message passing
+    println!("\nchannel!\n");
     let (tx, rx) = mpsc::channel();
     let tx1 = mpsc::Sender::clone(&tx);
     thread::spawn(move || {
@@ -27,21 +29,42 @@ fn main() {
         tx.send(val).unwrap();
     });
 
-thread::spawn(move || {
-    let vals = vec![
-        String::from("hi"),
-        String::from("from"),
-        String::from("the"),
-        String::from("thread"),
-    ];
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
 
-    for val in vals {
-        tx1.send(val).unwrap();
-        thread::sleep(Duration::from_secs(1));
-    }
-});
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
 
     for received in rx {
         println!("Got: {}", received);
     }
+
+    // 16.3 mutex
+    println!("\nmutex!\n");
+    let counter = Arc::new(Mutex::new(0)); // Mutex<T> provides intirior mutablility
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let _counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = _counter.lock().unwrap();
+            *num += 1;
+        });
+
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
